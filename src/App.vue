30d2fa54f46d58aaab2735cff52bfa36784b3ca6
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-04-19 09:38:57 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Header msg="Gabriel" />
+  </div>
+</template>
+<script src="https://kit.fontawesome.com/fe3e03610d.js" crossorigin="anonymous"></script>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="header">
+    <div class="header-block">
+      <i class="fa fa-question-circle"></i>
+      <font-awesome-icon :icon="['fas', 'question-circle']" size="2x" />
+      Ajuda
+    </div>
+    <div class="header-block">
+      <p class="header-block--logo">
+
+      Logo
+      </p>
+    </div>
+    <div class="header-block">
+      <font-awesome-icon :icon="['fas', 'user-circle']" size="2x" />
+      Conta
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// USAR BEM SEMPRE
+.header {
+  // positioning
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  
+
+  // colors
+  color: $secondary-blue;
+
+  // font formating
+  font-weight: 600;
+
+  &-block {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+    padding: 5px;
+    grid-gap: 5px;
+
+    &--logo {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      border-right: 3px solid $lightgray-border;
+      border-left: 3px solid $lightgray-border;
+    }
+  }
+}
+</style>
