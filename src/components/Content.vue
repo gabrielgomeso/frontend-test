7fdb52e81a57bb7c83be09e25f8bd42542b52e71
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-04-27 15:36:08 -0300

    Finish first page styling

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -8,14 +8,14 @@
     <h1 class="content-title">Bolsas Favoritas</h1>
     <p class="content-description">Adicione bolsas de cursos e faculdade do seu interesse e receba atualizações com as melhores ofertas disponíveis.</p>
     
-    <div>
-      <div>Todos os semestres</div>
-      <div>1° semestre de 2021</div>
-      <div>2° semestre de 2021</div>
+    <div class="content-filter">
+      <div class="content-filter--option">Todos os semestres</div>
+      <div class="content-filter--option">1° semestre de 2021</div>
+      <div class="content-filter--option">2° semestre de 2021</div>
     </div>
 
-    <button>
-      <font-awesome-icon class="plus-icon" :icon="['fas', 'plus']" size="5x" />
+    <button class="add-course">
+      <font-awesome-icon class="plus-icon" :icon="['fas', 'plus-circle']" size="5x" />
       <h4>Adicionar bolsa</h4>
       <p>Clique para adicionar bolsas de cursos do seu interesse</p>
     </button>
@@ -43,12 +43,50 @@
   }
 
   &-description{
+    margin: 0 0 20px 0;
+
     font-size: 1.3rem;
   }
 
+  &-filter {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    font-weight: 600;
+    color: $secondary-blue;
+    font-size: 1.2rem;
+
+
+    margin: 0 0 20px 0;
+
+
+    &--option:first-child {
+      border-radius: 5px 5px 0 0;
+      background: $secondary-blue;
+      color: $gray-background;
+    }
+
+    &--option:last-child {
+      border-radius: 0 0 5px 5px;
+    }
+
+    &--option{
+      padding: 10px;
+      border: 1px solid $secondary-blue;
+    }
+  }
+  
+  .add-course{
+    border: 0;
+    background: $gray-background;
+    box-shadow: 1px 1px 4px 1px rgb(112, 112, 112);
+    padding: 25px;
+  }
+
   .plus-icon{
-    border: 5px solid $secondary-blue;
-    border-radius: 100%;
+    margin: 0 0 20px 0;
+    
     padding: 10px;
     color: $secondary-blue;
   }
