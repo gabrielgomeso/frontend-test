3175a21c754a23ddc0ab8569f1d8227a8c029596
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-05-01 17:13:35 -0300

    Add Modal and Modal styling

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -6,30 +6,64 @@
     </p>
 
     <h1 class="content-title">Bolsas Favoritas</h1>
-    <p class="content-description">Adicione bolsas de cursos e faculdade do seu interesse e receba atualizações com as melhores ofertas disponíveis.</p>
-    
+    <p class="content-description">
+      Adicione bolsas de cursos e faculdade do seu interesse e receba
+      atualizações com as melhores ofertas disponíveis.
+    </p>
+
     <div class="content-filter">
       <div class="content-filter--option">Todos os semestres</div>
       <div class="content-filter--option">1° semestre de 2021</div>
       <div class="content-filter--option">2° semestre de 2021</div>
     </div>
 
-    <button class="add-course">
-      <font-awesome-icon class="plus-icon" :icon="['fas', 'plus-circle']" size="5x" />
+    <button class="btn add-course" @click="showModal">
+      <font-awesome-icon
+        class="plus-icon"
+        :icon="['fas', 'plus-circle']"
+        size="5x"
+      />
       <h4>Adicionar bolsa</h4>
       <p>Clique para adicionar bolsas de cursos do seu interesse</p>
     </button>
-  </div>  
+
+    <Modal v-show="isModalVisible" @close="closeModal" />
+  </div>
 </template>
+<script>
+
+import Modal from "./Modal.vue";
+
+export default {
+  name: "Content",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>
+
 
 <style lang="scss">
-.content{
+.content {
   width: 100%;
   background-color: $gray-background;
 
   padding: 20px;
 
-  &-page{
+  &-page {
     font-weight: 600;
     color: $secondary-blue;
 
@@ -37,11 +71,11 @@
     margin: 0 0 15px 0;
   }
 
-  &-title{
+  &-title {
     margin: 0 0 10px 0;
   }
 
-  &-description{
+  &-description {
     margin: 0 0 20px 0;
 
     font-size: 1.3rem;
@@ -56,9 +90,7 @@
     color: $secondary-blue;
     font-size: 1.2rem;
 
-
     margin: 0 0 20px 0;
-
 
     &--option:first-child {
       border-radius: 5px 5px 0 0;
@@ -70,26 +102,25 @@
       border-radius: 0 0 5px 5px;
     }
 
-    &--option{
+    &--option {
       cursor: pointer;
       padding: 10px;
       border: 1px solid $secondary-blue;
       border-top: 0;
     }
   }
-  
-  .add-course{
+
+  .add-course {
     border: 0;
     background: $gray-background;
     box-shadow: 1px 1px 4px 1px rgb(112, 112, 112);
     padding: 25px;
     margin: 0 0 20px 0;
-
   }
 
-  .plus-icon{
+  .plus-icon {
     margin: 0 0 10px 0;
-    
+
     padding: 10px;
     color: $secondary-blue;
   }
---
+++ src/components/Modal.vue
@@ -0,0 +1,196 @@
+<script>
+export default {
+  name: "Modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <button
+        type="button"
+        class="btn-close"
+        @click="close"
+        aria-label="Close modal"
+      >
+        <font-awesome-icon :icon="['fas', 'times']" size="2x" />
+      </button>
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            <h2 class="modal-header--title">Adicionar bolsa</h2>
+            <p class="modal-header--description">
+              Filtre e adicione as bolsas de seu interesse.
+            </p>
+          </slot>
+        </header>
+
+        <section class="modal-body">
+          <slot name="body" id="modalDescription">
+            <label class="modal-label" for="cidade">SELECIONE SUA CIDADE</label>
+            <select class="modal-select" name="cidade" id="cidade">
+              <option value="sjc">São José</option>
+              <option value="carag">Caraguá</option>
+              <option value="sp">São Paulo</option>
+            </select>
+
+            <label class="modal-label" for="curso">SELECIONE SEU CURSO</label>
+            <select class="modal-select" name="curso" id="curso">
+              <option value="cs">Ciencia da Computacao</option>
+              <option value="med">Medicina</option>
+              <option value="sp">São Paulo</option>
+            </select>
+            <p class="modal-label">COMO VOCÊ QUER ESTUDAR?</p>
+            <label for="presencial">Presencial</label>
+            <input type="checkbox" id="presencial" name="presencial" checked />
+            <label for="adistancia">A distância</label>
+            <input type="checkbox" id="adistancia" name="adistancia" checked />
+
+            <p class="modal-label">ATÉ QUANTO PODE PAGAR?</p>
+          </slot>
+        </section>
+
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button
+              type="button"
+              class="btn"
+              @click="close"
+              aria-label="Cancel"
+            >
+              Cancelar
+            </button>
+
+            <button
+              type="button"
+              class="btn-disabled"
+              aria-label="Add scholarship"
+              disabled
+            >
+              Adicionar bolsa(s)
+            </button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+<style lang="scss">
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(31, 45, 48, 88%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  padding: 20px;
+  min-width: 100%;
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header {
+  position: relative;
+
+  &--title {
+    font-size: 2rem;
+    margin-bottom: 5px;
+  }
+
+  &--description {
+    font-size: 1.5rem;
+    margin-bottom: 1.5em;
+  }
+}
+
+.modal-footer {
+  padding-top: 10px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-body {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  position: relative;
+
+
+  .modal-label{
+    font-size: 1.1rem;
+    font-weight: 800;
+    letter-spacing: 0.5px;
+  }
+
+   .modal-select{
+     border-radius: 5px;
+     padding: 10px;
+     border: 1px solid #D6D9D9;
+
+
+   }
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  padding: 5px;
+  cursor: pointer;
+  color: white;
+  background: transparent;
+}
+
+.btn {
+  color: $secondary-blue;
+  background: #fff;
+  border: 1px solid $secondary-blue;
+  border-radius: 2px;
+  font-weight: 600;
+  padding: 15px 15px 15px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn-disabled {
+  color: #82898B;
+  background: #CACDCE;
+  border: 1px solid #82898B;
+  border-radius: 2px;
+  font-weight: 600;
+  padding: 15px 15px 15px 15px;
+  border-radius: 5px;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
+
